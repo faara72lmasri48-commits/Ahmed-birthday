c5498100af145ad8f32fc137e0edd078e218b03e
faara72lmasri48-commits <faara72lmasri48@gmail.com>
2026-05-31 00:50:15 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("Ahmed Birthday");
+function checkPassword(){
+
+    let password =
+    document.getElementById("password").value;
+
+    if(password === "Ahmed26"){
+
+        document
+        .getElementById("screen1")
+        .classList.remove("active");
+
+        document
+        .getElementById("screen2")
+        .classList.add("active");
+
+    }
+
+    else{
+
+        document
+        .getElementById("error")
+        .innerHTML =
+        "Wrong Secret Code ❤️";
+
+    }
+}
+
+function nextScreen(number){
+
+    document
+    .querySelectorAll(".screen")
+    .forEach(screen => {
+
+        screen.classList.remove("active");
+
+    });
+
+    document
+    .getElementById("screen" + number)
+    .classList.add("active");
+
+}
